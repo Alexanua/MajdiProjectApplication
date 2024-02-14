e7b94e5ea24f4ba813e4459559446393f6301682
Alexanua <86alesha@gafe.molndal.se>
2024-02-14 11:38:06 +0100

    update

--- src/test/java/com/Majdi/Majdi_Project/MajdiProjectApplicationTests.java
+++ src/test/java/com/Majdi/Majdi_Project/MajdiProjectApplicationTests.java
@@ -1,20 +1,7 @@
 package com.Majdi.Majdi_Project;
-/*
-import org.junit.jupiter.api.Test;
+
 import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MajdiProjectApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
-}
-
- */
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MajdiProjectApplicationTests {
